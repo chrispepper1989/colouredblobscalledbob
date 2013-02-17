dff136115cb3ba252543a0081d21ec419b24c247
OpenShift guest <chrispepper1989@gmail.com>
2013-02-17 17:22:22 -0500

    submitting folder structure and demo files

---
+++ Javascript/Framework/TileGrid/Tile.js
@@ -0,0 +1,12 @@
+function Tile()
+{
+    
+}
+
+Tile.prototype.getInfo = function()
+{
+    return this.color + ' ' + this.type + ' apple';
+};
+
+
+
---
+++ Javascript/Framework/TileGrid/TileGrid.js
@@ -0,0 +1,9 @@
+function TileGrid()
+{
+    
+}
+
+TileGrid.prototype.Draw = function(dt)
+{
+    return this.color + ' ' + this.type + ' apple';
+};
